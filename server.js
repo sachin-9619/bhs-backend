568d94c710e93f56bdbb8ac82f57bff2b6546581
sachin-9619 <sachinmore85395@gmail.com>
2026-01-06 12:18:18 +0530

    lm

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
 require("dotenv").config();
+const app = require("./app");
 
 const app = express();
 app.use(express.json());
