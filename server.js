46481ead1731364dc481dfe10a269797a0cddb41
sachin-9619 <sachinmore85395@gmail.com>
2026-01-07 11:55:11 +0530

    lki

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const app = require("./app");
-const startBookingReminderCron = require("./bookingReminderCron");
+
+// side effects AFTER load
+require("./db");
+require("./bookingReminderCron");
 
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 listening");
-
-  // ✅ start cron AFTER server is ready
-  startBookingReminderCron();
 });
