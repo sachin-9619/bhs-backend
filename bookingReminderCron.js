08523ad4780604a610d8aa7671eed7c214e6678a
sachin-9619 <sachinmore85395@gmail.com>
2026-01-07 11:53:15 +0530

    sfh

--- bookingReminderCron.js
+++ bookingReminderCron.js
@@ -1,5 +1,5 @@
 const cron = require("node-cron");
-const db = require("../db");
+const db = require("./db");
 const { sendBookingMail } = require("./mailer");
 
 cron.schedule("*/10 * * * *", async () => {
