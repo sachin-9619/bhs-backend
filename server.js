cda775fc13719242f4a2231c03833068cc95b9d8
sachin-9619 <sachinmore85395@gmail.com>
2026-01-07 11:47:41 +0530

    frt

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 listening");
-  startBookingReminderCron(); // 🔥 AFTER server is live
+
+  // ✅ start cron AFTER server is ready
+  startBookingReminderCron();
 });
