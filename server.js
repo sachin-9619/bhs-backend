92eccc7997623201b6cdc4f16e7f26defb0075c2
sachin-9619 <sachinmore85395@gmail.com>
2026-01-08 21:40:45 +0530

    vsj

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const app = require("./app");
 const { initDB } = require("./db");
+const { getTransporter } = require("./mailer"); // nodemailer setup file
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,6 +10,11 @@ const PORT = process.env.PORT || 5000;
   try {
     await initDB();
     console.log("✅ DB ready");
+
+    const mailer = getTransporter();
+    await mailer.verify();
+    console.log("✅ Mail server connected");
+
 
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`🚀 Server running on port ${PORT}`);
