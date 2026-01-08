21de3c49b9b6c0b279d1ef1ef75729d934cbddd2
sachin-9619 <sachinmore85395@gmail.com>
2026-01-08 19:04:56 +0530

    bgdjh

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,21 @@
 // routes/booking.js
 const express = require("express");
 const router = express.Router();
-const routeController = require("../controllers/routeController");
+const bookingController = require("../controllers/bookingController");
 
 // ================= BOOKED SEATS =================
-router.get("/:routeId/seats", routeController.getBookedSeats);
+router.get("/:routeId/seats", bookingController.getBookedSeats);
 
 // ================= BOOK SEATS =================
-router.post("/:routeId", routeController.bookSeats);
+router.post("/:routeId", bookingController.bookSeats);
 
 // ================= GET BOOKING BY ID =================
-router.get("/:id", routeController.getBookingById);
+router.get("/:id", bookingController.getBookingById);
 
 // ================= ADMIN VIEW =================
-router.get("/", routeController.getAllBookingsForAdmin);
+router.get("/", bookingController.getAllBookingsForAdmin);
 
 // ================= DELETE =================
-router.delete("/:id", routeController.deleteBooking);
+router.delete("/:id", bookingController.deleteBooking);
 
 module.exports = router;
