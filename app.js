25f4b8d060a4dc7b6264c01bb5c51efd6aeae472
sachin-9619 <sachinmore85395@gmail.com>
2026-01-08 13:13:57 +0530

    swe

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 
-// const routeRoutes = require("./routes/routes");
-// const bookingRoutes = require("./routes/booking");
-// const contactRoutes = require("./routes/contact");
-// const adminRoutes = require("./routes/admin");
+const routeRoutes = require("./routes/routes");
+const bookingRoutes = require("./routes/booking");
+const contactRoutes = require("./routes/contact");
+const adminRoutes = require("./routes/admin");
 
 const app = express();
 
@@ -16,10 +16,10 @@ app.get("/", (req, res) => {
   res.status(200).send("🚀 BHS Backend is LIVE");
 });
 
-// // ✅ API ROUTES
-// app.use("/api/routes", routeRoutes);
-// app.use("/api/booking", bookingRoutes);
-// app.use("/api/contact", contactRoutes);
-// app.use("/api/admin", adminRoutes);
+// ✅ API ROUTES
+app.use("/api/routes", routeRoutes);
+app.use("/api/booking", bookingRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/admin", adminRoutes);
 
 module.exports = app;
